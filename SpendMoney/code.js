18b03a302abc02cda3a23150f40848f2f973e252
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-02-17 22:44:09 +0300

    add automatic card rendering and created new product cards

--- SpendMoney/code.js
+++ SpendMoney/code.js
@@ -1,1 +1,27 @@
-const money = 20000000000000;
+const productsEl = document.querySelector(".cards");
+
+
+function renderCard() {
+  products.forEach((product) => {
+    productsEl.innerHTML += `
+                        <div class="card">
+                        <div class="cardDescription">
+                          <div class="cardImage"> 
+                            <img src=${product.image}></div>
+                        </div>
+                        <div class="cardBottom">
+                          <div class="cardPrice "> ${product.price}</div>
+                          <div class="cardTitle"> ${product.name}</div>
+                          <div class="btns">
+                            <button class="cardDelete" onclick="delete(${product.id})">-</button>
+                            <div class="counter">0</div>
+                            <button class="cardAdd" onclick="add(${product.id})">+</button>
+                          </div>
+                      </div>
+                    </div>
+        `;
+  });
+
+}
+
+renderCard();
