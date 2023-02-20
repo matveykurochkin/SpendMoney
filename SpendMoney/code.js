0a09daa2a9477aa65dff72709822375becfc8291
Matvey Kurochkin <matvey.kurochkin@gmail.com>
2023-02-20 19:08:39 +0300

    add logic pay and sell product and add create your shopping list

--- SpendMoney/code.js
+++ SpendMoney/code.js
@@ -1,5 +1,13 @@
-const productsEl = document.querySelector(".cards");
+let money = 20000000000000;
+let sMoney = 0;
+let limitProduct = 500;
 
+const productsEl = document.querySelector(".cards");
+let totalMoney = document.querySelector(".totalMoney");
+let spendMoney = document.querySelector(".spendMoney");
+let productInReceipt = document.querySelector(".productInReceipt");
+let resultPriceReceipt = document.querySelector(".resultPriceReceipt");
+let saveButton = document.querySelector('.saveButton');
 
 function renderCard() {
   products.forEach((product) => {
@@ -13,15 +21,73 @@ function renderCard() {
                           <div class="cardPrice "> ${product.price}</div>
                           <div class="cardTitle"> ${product.name}</div>
                           <div class="btns">
-                            <button class="cardDelete" onclick="delete(${product.id})">-</button>
-                            <div class="counter">0</div>
-                            <button class="cardAdd" onclick="add(${product.id})">+</button>
+                            <button class="cardDelete" onclick="deleteCard(${product.id})">-</button>
+                            <div class="counter+${product.id}">${product.count}</div>
+                            <button class="cardAdd" onclick="addCard(${product.id})">+</button>
                           </div>
                       </div>
                     </div>
         `;
   });
-
 }
 
 renderCard();
+
+function renderReceipt() {
+  productInReceipt.innerHTML = "";
+  products.forEach((product) => {
+    if(product.count > 0){
+      productInReceipt.innerHTML += `<p> ${product.name} X${product.count}  ..........  ${product.price} ₽ \n</p>`;
+    }
+  });
+}
+
+function deleteCard(id){
+  products.forEach((product) => {
+    if(id == product.id && product.count > 0){
+
+     product.count --;
+     money += product.price;
+     sMoney -= product.price;
+
+     spendMoney.innerHTML = "Потрачено: " + sMoney;
+     totalMoney.innerHTML = "Всего: " + money; 
+     resultPriceReceipt.innerHTML = "Итого: " + sMoney;
+
+     renderReceipt();
+     productsEl.innerHTML = "";
+     renderCard();
+    }
+   });
+}
+
+function addCard(id){
+  products.forEach((product) => {
+    if(id == product.id  && product.count < limitProduct && product.price <= money){
+
+     product.count ++;
+     money -= product.price;
+     sMoney += product.price;
+
+     totalMoney.innerHTML = "Всего: " + money;
+     spendMoney.innerHTML = "Потрачено: " + sMoney;
+     resultPriceReceipt.innerHTML = "Итого: " + sMoney;
+
+     renderReceipt();
+
+     productsEl.innerHTML = "";
+     renderCard();
+    }
+   });
+}
+
+function saveInFile(){
+  let text = document.querySelector('.receipt').textContent;
+  let file = document.createElement('a');
+  file.setAttribute('href', `data:text/plain;charset=utf-8,${encodeURIComponent(text)}`);
+  file.setAttribute('download', 'Your Receipt.txt');
+  file.style.display = 'none';
+  document.body.appendChild(file);
+  file.click();
+  document.body.removeChild(file);
+}
--- SpendMoney/products.js
+++ SpendMoney/products.js
@@ -3,91 +3,106 @@ const products = [
         id: 1,
         name: "Lamborghini Urus",
         price: 49000000,
-        image: "https://ivanglazunov.ru/wp-content/uploads/6/8/7/68746e9691c2bd63856b99833ca78742.jpeg"
+        image: "https://ivanglazunov.ru/wp-content/uploads/6/8/7/68746e9691c2bd63856b99833ca78742.jpeg",
+        count: 0
     },
     {
         id: 2,
         name: "Apple iPhone 14 Pro Max",
         price: 130000,
-        image: "https://cdn.citilink.ru/-zqOm3tjcgGdnJ-3qLjO3EglNlIMYVIKBi0FOCr4qk4/resizing_type:fit/gravity:sm/width:1200/height:1200/plain/items/1895984_v01_b.jpg"
+        image: "https://cdn.citilink.ru/-zqOm3tjcgGdnJ-3qLjO3EglNlIMYVIKBi0FOCr4qk4/resizing_type:fit/gravity:sm/width:1200/height:1200/plain/items/1895984_v01_b.jpg",
+        count: 0
     },
     {
         id: 3,
         name: "Apple AirPods Pro",
         price: 16000,
-        image: "https://avatars.mds.yandex.net/get-mpic/5241173/img_id7095765708916818250.jpeg/orig"
+        image: "https://avatars.mds.yandex.net/get-mpic/5241173/img_id7095765708916818250.jpeg/orig",
+        count: 0
     },
     {
         id: 4,
         name: "Samsung Galaxy S23 Ultra",
         price: 147900,
-        image: "https://avatars.mds.yandex.net/get-mpic/1336510/img_id5193470031453206464.png/orig"
+        image: "https://avatars.mds.yandex.net/get-mpic/1336510/img_id5193470031453206464.png/orig",
+        count: 0
     },
     {
         id: 5,
         name: "PlayStation 5",
         price: 62999,
-        image: "https://c.dns-shop.ru/thumb/st1/fit/0/0/046bc1ff14b43a741f6f93a90206a248/1a0124c174c38b4258bc826bbefa9bac880e59d27b57dbb29afbd55c2511f9e1.jpg.webp"
+        image: "https://c.dns-shop.ru/thumb/st1/fit/0/0/046bc1ff14b43a741f6f93a90206a248/1a0124c174c38b4258bc826bbefa9bac880e59d27b57dbb29afbd55c2511f9e1.jpg.webp",
+        count: 0
     },
     {
         id: 6,
         name: "Xbox Series X",
         price: 66990,
-        image: "https://avatars.mds.yandex.net/get-mpic/5323566/img_id6472143200699646946.jpeg/orig"
+        image: "https://avatars.mds.yandex.net/get-mpic/5323566/img_id6472143200699646946.jpeg/orig",
+        count: 0
     },
     {
         id: 7,
         name: "Apple Macbook Pro 16",
         price: 190790,
-        image: "https://avatars.mds.yandex.net/get-mpic/5032583/img_id3683841579270874537.jpeg/orig"
+        image: "https://avatars.mds.yandex.net/get-mpic/5032583/img_id3683841579270874537.jpeg/orig",
+        count: 0
     },
     {
         id: 8,
         name: "Apple iPad Air 2022",
         price: 79990,
-        image: "https://cdn.citilink.ru/otHSmZbbasI_xm7tSjV-xo9lFWeZCufwyrueh5EAiPQ/resizing_type:fit/gravity:sm/width:1200/height:1200/plain/items/1848556_v01_b.jpg"
+        image: "https://cdn.citilink.ru/otHSmZbbasI_xm7tSjV-xo9lFWeZCufwyrueh5EAiPQ/resizing_type:fit/gravity:sm/width:1200/height:1200/plain/items/1848556_v01_b.jpg",
+        count: 0
     },
     {
         id: 9,
         name: "Bugatti Veyron",
         price: 112200000,
-        image: "https://autonewsmake.ru/wp-content/uploads/2016/12/bugatti-veyron-16-4.jpg"
+        image: "https://autonewsmake.ru/wp-content/uploads/2016/12/bugatti-veyron-16-4.jpg",
+        count: 0
     },
     {
         id: 10,
         name: "Rolex Datejust",
         price: 980000,
-        image: "https://avatars.mds.yandex.net/get-goods_pic/6413880/picd143e10d8db91dbc22e8d3c1e1f46849/orig"
+        image: "https://avatars.mds.yandex.net/get-goods_pic/6413880/picd143e10d8db91dbc22e8d3c1e1f46849/orig",
+        count: 0
     },
     {
         id: 11,
         name: "Mega Yatch",
-        price: 358848000000,
-        image: "https://i.pinimg.com/originals/b5/97/42/b59742cefa4c5f3c1447c4a8a439035d.jpg"
+        price: 22428000000,
+        image: "https://i.pinimg.com/originals/b5/97/42/b59742cefa4c5f3c1447c4a8a439035d.jpg",
+        count: 0
     },
     {
         id: 12,
-        name: "Monalisa by Leonardo da Vinci",
+        name: "Monalisa by da Vinci",
         price: 53827200000,
-        image: "https://enotnavolge.ru/wp-content/uploads/e/d/a/edae373ffd2a36f75dd9a183907d6f97.jpeg"
+        image: "https://enotnavolge.ru/wp-content/uploads/e/d/a/edae373ffd2a36f75dd9a183907d6f97.jpeg",
+        count: 0
     },
     {
         id: 13,
         name: "Tesla Model S Plaid",
         price: 9868320,
-        image: "https://motor.ru/imgs/2021/07/02/12/4746281/f7224b7480f0f8c4943a990c043da89b3027764f.jpg"
+        image: "https://motor.ru/imgs/2021/07/02/12/4746281/f7224b7480f0f8c4943a990c043da89b3027764f.jpg",
+        count: 0
     },
     {
         id: 14,
         name: "Airbus A380",
         price: 44856000000,
-        image: "https://regnum.ru/uploads/pictures/news/2016/10/11/regnum_picture_147618588576860_normal.jpg"
+        image: "https://regnum.ru/uploads/pictures/news/2016/10/11/regnum_picture_147618588576860_normal.jpg",
+        count: 0
     },
     {
         id: 15,
         name: "RTX 4090",
         price: 247000,
-        image: "https://avatars.mds.yandex.net/get-mpic/7150546/img_id3418954759661600105.jpeg/orig"
+        image: "https://avatars.mds.yandex.net/get-mpic/7150546/img_id3418954759661600105.jpeg/orig",
+        count: 0
     }
 
 ];
